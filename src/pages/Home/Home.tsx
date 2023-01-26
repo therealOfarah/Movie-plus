b71c3425cc0bd4119cd2c53f1675e006d6c8a8a1
Omar Farah <omaarfaarah2019@gmail.com>
2023-01-26 14:44:18 -0500

    update UI for movies and shows

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,14 +12,21 @@ function Home(movie:any) {
     <div id="card-container">
       {data?.map((m:any)=>
         <>
-        <div className="card" style={{width: "18rem"}} key={m.id} >
-          <img src={`${`https://image.tmdb.org/t/p/w440_and_h660_face/${m.backdrop_path}`}`}  className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">{ m.original_name === undefined ? m.title:m.name}</h5>
-            <p className="card-text">{m.release_date === undefined ? m.first_air_date:m.release_date}</p>
-            <Link to={`/movie/${m.id}`} state={m}><button className="btn btn-primary">Continue</button></Link>
+        <div className="movie-card">
+        <Link to={`/movie/${m.id}`} state={m}>
+          <img
+            className="img-fluid movie-img"
+            src={`${`https://image.tmdb.org/t/p/w440_and_h660_face/${m.backdrop_path}`}`}
+            alt="Sorry, something went wrong"
+            />
+          </Link>
+            <div
+              className="movie-description p-3 d-flex justify-content-between align-items-center"
+            >
+              <h5 className="movie-title">{ m.original_name === undefined ? m.title:m.name}</h5>
+              <p className="card-text">{m.release_date === undefined ? m.first_air_date:m.release_date}</p>
+            </div>
           </div>
-        </div>
         </>
         )}
     </div>
